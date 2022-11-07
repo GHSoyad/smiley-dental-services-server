609b10b8e06e5743532ae4f070be4b45f2abfc9a
GHSoyad <ghsoyad@gmail.com>
2022-11-07 22:24:44 +0600

    Added vercel deploy setup

--- index.js
+++ index.js
@@ -6,6 +6,15 @@ app.get('/', (req, res) => {
     res.send("Server is running");
 })
 
+app.get('/test', (req, res) => {
+    const testData = [
+        {
+            test1: "testing data"
+        }
+    ]
+    res.send(testData)
+})
+
 app.listen(port, () => {
     console.log(`App is listening on port ${port}`)
 })
