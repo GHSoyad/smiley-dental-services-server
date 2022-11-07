3d65b7e8798162eccee2f021a0ee1fcbfc81bb4a
GHSoyad <ghsoyad@gmail.com>
2022-11-08 00:27:51 +0600

    Added services data and all services get method

--- index.js
+++ index.js
@@ -3,13 +3,11 @@ const app = express();
 const port = process.env.PORT || 5000;
 const cors = require('cors');
 require('dotenv').config();
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
 //Middlewares
 app.use(cors());
 app.use(express.json());
-
-
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.7r6jn89.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
@@ -18,23 +16,32 @@ async function run() {
     try {
         const servicesCollection = client.db("smiley-dental-services").collection("services");
 
-        const service = {
-            name: "Dental Crown",
-            img: "https://i.ibb.co/qgZMHC5/dental-crown.jpg",
-            cost: 500,
-            description: "A dental crown is a dental prosthesis which replaces the visible part of a tooth. A dental crown functions to strengthen teeth, restore their original shape, and improve their appearance. Dental crowns are also used to hold dental bridges in place and cover dental implants.",
-            rating: 4.4
-        }
+        app.post('/services', async (req, res) => {
+            const service = {
+                name: "Dental Crown",
+                img: "https://i.ibb.co/qgZMHC5/dental-crown.jpg",
+                cost: 500,
+                description: "A dental crown is a dental prosthesis which replaces the visible part of a tooth. A dental crown functions to strengthen teeth, restore their original shape, and improve their appearance. Dental crowns are also used to hold dental bridges in place and cover dental implants.",
+                rating: 4.4
+            }
 
-        const result = await servicesCollection.insertOne(service);
+            const result = await servicesCollection.insertOne(service);
+            console.log(result);
+        })
 
-        console.log(result);
+        app.get('/services', async (req, res) => {
+            const query = {};
+            const cursor = servicesCollection.find(query);
+            const result = await cursor.toArray();
+            res.send(result);
+            console.log(result)
+        })
 
     } finally {
+
     }
 }
-// run().catch(error => console.log(error));
-
+run().catch(error => console.log(error));
 
 
 app.get('/', (req, res) => {
